181bc62487d50ad9b10e0ec5415d7b527793eba1
OlegKryuchkov <kryuchkov.oleg@gmail.com>
2024-02-02 18:38:00 +0200

    ready to check

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(key: Key) {
+    key = key;
+  }
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("можна заходити");
+    } else {
+      console.log("не можна заходити");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("вірний ключ! двері відчинені!");
+    } else {
+      this.door = false;
+      console.log("не вірний ключ! двері зачинені!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +50,4 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
-
-export {};
+export { Key };
